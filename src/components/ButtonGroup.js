b2dcce3b97e153176ead571b29e0dfb98c096069
Thomas <pyristfeuer@gmail.com>
2023-03-02 15:54:20 +0100

    added change handler + useState

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -2,10 +2,15 @@ import { Row, Col, Button } from "react-bootstrap";
 import './ButtonGroup.css';
 
 function ButtonGroup(props) {
+
+	function buttonPressedHandler(event) {
+		console.log("pressed!");
+	}	
+
   return (
     <Row>
       <Col className="d-flex justify-content-center">
-        <Button className="btn-secondary">Check Text!</Button>
+        <Button className="btn-secondary" onClick={buttonPressedHandler}>Check Text!</Button>
       </Col>
     </Row>
   );
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,12 +1,24 @@
+import {useState} from 'react';
 import {Row, Col} from 'react-bootstrap';
 import './TextArea.css';
 
 function TextArea(props) {
+
+	const [wordLength, setWordLength] = useState(0);
+
+	function textChangeHandler(event) {
+		setWordLength(event.target.value.length);
+	}
+
   return (
     <Row>
       <Col className="d-flex justify-content-center">
-        <textarea className="main-text-area"></textarea>
+        <textarea className="main-text-area" onChange={textChangeHandler}/>
+				
       </Col>
+			<Col className="d-flex justify-content-center">
+				<p className="word-count">Word count: {wordLength}</p>
+			</Col>
     </Row>
   );
 }
